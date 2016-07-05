f987097bc1fe45808c1ba2d9e0622a1014dfcad0
MrCharif <mrcharifmakaoui@gmail.com>
2016-07-05 04:24:58 +0000

    Add Content Area and Team List

--- scripts/global.js
+++ scripts/global.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
    $('.menu_button').click(function () {
        $('.Side_nave').toggleClass( "hide_side show_side" );
        $('.Header_nav').toggleClass( "min_nav full_nav" );
+       $('.content').toggleClass( "min_content full_content" );
    })
 });
